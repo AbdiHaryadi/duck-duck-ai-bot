3a52bd870b16e93b4aafbd3c2c2858247f7feb00
naufalizza <box.naufalizza@gmail.com>
2021-12-02 22:15:26 +0700

    feat: building and update_border

--- simple/util/building.py
+++ simple/util/building.py
@@ -1,4 +1,12 @@
-def building(game_state, worker, border_list):
+from ..lux.game_objects import *
+from ..lux.game_map import *
+from ..lux.game import *
+
+from .update_border import *
+
+import random
+
+def building(game_state: Game, worker: Unit, border_list: list[Cell]):
     """
     Go to nearest border_list tile if not there.
     Build if there.
@@ -7,4 +15,23 @@ def building(game_state, worker, border_list):
     Return action if not build a city_tile yet in border_list
     Return None if yes
     """
-    return None
+    if worker.get_cargo_space_left() > 0:
+        return "GO_FARMING"
+
+    nearest_border: Cell = None
+    for border in border_list:
+        if nearest_border == None:
+            nearest_border = border
+        else:
+            if worker.pos.distance_to(border.pos) < worker.pos.distance_to(nearest_border.pos):
+                nearest_border = border
+
+    if worker.pos.equals(nearest_border.pos):
+        if worker.can_build(game_state.map):
+            border_list = update_border(game_state, border_list, nearest_border)
+            return None
+        else:
+            random_border = border_list[random.randint(0, len(border_list)-1)]
+            return worker.move(worker.pos.direction_to(random_border.pos))
+    """WARNING: 'direction_to()' need to be changed into A* based direction finder"""
+    return worker.move(worker.pos.direction_to(nearest_border.pos))
