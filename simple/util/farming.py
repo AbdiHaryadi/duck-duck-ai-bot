645ee5aa7d3893b611950acbe4438ad301c3c77d
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2021-12-02 16:07:47 +0700

    feat: functional farming and scan_initial_border

--- simple/util/farming.py
+++ simple/util/farming.py
@@ -1,10 +1,36 @@
-def farming(game_state, worker, resource_list):
+from lux.constants import Constants
+
+def farming(game_state, player, worker, resource_list):
     """
     Go to nearest resource_list tile if not there.
-    Do nothing if there. (use action.move center, not None)
-    Precondition: worker_id has 100 wood
+    Do nothing if there. (return move center action, not None)
+    Precondition: worker have less than 100 wood
     
-    Return action if wood < 100 (resource full)
-    Return None if wood == 100
+    Return action if wood < 100
+    Return None if wood == 100 (resource full)
     """
-    return None
+    # Note that resource_list is cells
+    if worker.cargo.wood == 100:
+        return None
+    else:
+        tile_distance = lambda t: t.pos.distance_to(worker.pos)
+        gatherable_list = list(filter(
+            lambda r: gatherable(player, r.resource.type),
+            resource_list
+        ))
+        selected_resource = min(gatherable_list, key=tile_distance)
+        selected_direction = worker.pos.direction_to(selected_resource.pos)
+        
+        return worker.move(selected_direction)
+    
+def gatherable(player, resource_type):
+    if resource_type == Constants.RESOURCE_TYPES.WOOD:
+        return True
+    elif resource_type == Constants.RESOURCE_TYPES.COAL:
+        return player.researched_coal()
+    elif resource_type == Constants.RESOURCE_TYPES.URANIUM:
+        return player.researched_uranium()
+    else:
+        # Unknown resource
+        print("Warning: unknown resource: {}".format(resource_type))
+        return False
